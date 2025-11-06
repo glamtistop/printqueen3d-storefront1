340ed908f1834d35b0e3fb7d8c03614ae9b5dd34
Nandi Nelson <nandinelson@Nandis-MacBook-Air.local>
2025-11-06 00:44:04 -0800

    Update JotForm to use iframe embed method for better compatibility

--- src/app/[countryCode]/(main)/partnership/page.tsx
+++ src/app/[countryCode]/(main)/partnership/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useEffect } from "react"
 import Script from "next/script"
 
 export default function PartnershipPage() {
@@ -25,13 +24,29 @@ export default function PartnershipPage() {
               Pitch product ideas, event activations, or content collabs. Fill out the form below and we'll get back to you within 48 hours.
             </p>
 
-            {/* JotForm Embed */}
+            {/* JotForm iFrame Embed */}
             <div className="jotform-container">
+              <iframe
+                id="JotFormIFrame-253091307365051"
+                title="Partnership and Collaboration Request Form"
+                allowTransparency={true}
+                allow="geolocation; microphone; camera; fullscreen; payment"
+                src="https://form.jotform.com/253091307365051"
+                style={{
+                  minWidth: '100%',
+                  maxWidth: '100%',
+                  height: '539px',
+                  border: 'none'
+                }}
+                scrolling="no"
+              />
               <Script
-                type="text/javascript"
-                src="https://form.jotform.com/jsform/253091307365051"
+                src="https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js"
                 strategy="afterInteractive"
               />
+              <Script id="jotform-handler" strategy="afterInteractive">
+                {`window.jotformEmbedHandler("iframe[id='JotFormIFrame-253091307365051']", "https://form.jotform.com/")`}
+              </Script>
             </div>
 
             {/* Contact Info */}
